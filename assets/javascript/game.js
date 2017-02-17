68749b9631929b5ca95b610c0b280be13efdafc4
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-02-17 18:09:31 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,13 @@ var currentWordOutput = document.getElementById("current-word");
 // picks random character from word array
 var currentWord = words[Math.floor(Math.random() * words.length)];
 
-// currentWordOutput.innerHTML = currentWord;
-
 var wordLength = currentWord.length;
 
-var output = "";
-
-for (var f = 0; f < currentWord.length; f++){
-	output += "_";
-}
-
-
-// creates the dashed "hidden" word output
-for (var i = 0; i < wordLength; i++) {
-	currentWordOutput.innerHTML += "_";
+for(var a = 0; a < wordLength + 1; a++){
+	var newSpan = document.createElement('span');	
+	newSpan.id = a;
+	// newSpan = "_";
+	currentWordOutput.innerHTML += newSpan;
 }
 
 
@@ -34,31 +27,18 @@ console.log("here");
 
 document.onkeyup = function(event){
 
-	// if (guess === currentWord.) {}
-	// document.getElementById("")
 var guess = event.key;
-var letersInsideWord = [];
 
-// puts each letter in a seperate string inside of the array letersInsideWord
-for (var x = 0; x < wordLength; x++){
-	letersInsideWord.push(currentWord.charAt(x));
+for(var b = 0; b < wordLength; b++){
+	var spanId = document.getElementById(b.toString());
+
+if (guess === currentWord.charAt(b)) {
+	spanId.innerHTML = currentWord.charAt(b);
+
+
 }
 
-// for (var n = 0; n < letersInsideWord.length; n++){
-// 	if (guess === letersInsideWord[n]){
-// 		output[n] = letersInsideWord[n];
-// 		currentWordOutput.innerHTML = output;
-// 	}
-	
-// 	}
+}
 
-
-for (var n = 0; n < currentWord.length; n++){
-	if (guess === currentWord.slice(n, n + 1)){
-		
-		output.charAt(n) = guess;
-	}
-	
-	}
 }
 console.log("here");
